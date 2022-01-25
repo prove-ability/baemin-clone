421846eefaec283ec79af89e700661fb1d650b63
Bard <prove.ability00@gmail.com>
2022-01-25 16:32:56 +0900

    🏷️ 버튼 컴포넌트 타입 추가

--- components/common/button/index.tsx
+++ components/common/button/index.tsx
@@ -1,5 +1,17 @@
-function button() {
-	return <div>button</div>;
+import { ButtonHTMLAttributes } from "react";
+import { buttonRecipe } from "./button.css";
+import { recipe } from "@vanilla-extract/recipes";
+
+interface Props extends Pick<ButtonHTMLAttributes<HTMLButtonElement>, "type"> {
+	recipe?: "default";
 }
 
-export default button;
+const Button: React.FC<Props> = ({ children, type = "button", recipe = "default" }) => {
+	return (
+		<button className={buttonRecipe({ type: recipe })} type={type}>
+			{children}
+		</button>
+	);
+};
+
+export default Button;
