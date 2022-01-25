ca634639f69413e3d9871a2639353b786b5a6eff
Bard <prove.ability00@gmail.com>
2022-01-25 16:29:55 +0900

    🔧 nextjs 설정 파일 수정

--- next.config.js
+++ next.config.js
@@ -1,4 +1,4 @@
-import { createVanillaExtractPlugin } from "@vanilla-extract/next-plugin";
+const { createVanillaExtractPlugin } = require("@vanilla-extract/next-plugin");
 const withVanillaExtract = createVanillaExtractPlugin();
 
 /** @type {import('next').NextConfig} */
