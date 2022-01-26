84938ffab7cf1e86e9fe57df67ee16d5488ec103
Bard <prove.ability00@gmail.com>
2022-01-26 19:32:32 +0900

    💄 app.tsx reset.css import

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,7 @@
-import "../styles/globals.css";
-import type { AppProps } from "next/app";
-import Layout from "../components/layout";
+import "styles/reset.css";
 import { useEffect } from "react";
+import type { AppProps } from "next/app";
+import Layout from "components/layout";
 import { darkTheme, lightTheme } from "styles/themes.css";
 
 function cleanThemes() {
