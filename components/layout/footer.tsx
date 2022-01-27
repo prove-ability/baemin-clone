8c5d9bf3a0af64b56b95c1708e506b61333d4802
Bard <prove.ability00@gmail.com>
2022-01-27 22:34:51 +0900

    ✨ footer 구조 작업

--- components/layout/footer.tsx
+++ components/layout/footer.tsx
@@ -1,5 +1,21 @@
+import { footerRecipe } from "styles/components/layout/footer.css";
+
 function Footer() {
-	return <footer>Footer</footer>;
+	return (
+		<footer className={footerRecipe()}>
+			{/* 케러셀 */}
+			<div>케러셀</div>
+			{/* 약관들 */}
+			<ul>
+				<li>사업자정보확인</li>
+				<li>이용약관</li>
+				<li>전자금용거래이용약관</li>
+				<li>개인정보처리방침</li>
+			</ul>
+			{/* 회사 정보 */}
+			(주)우아한형제들
+		</footer>
+	);
 }
 
 export default Footer;
