3ba1c50a941d7e395d1ed536eb66522ec4de89cf
Bard <prove.ability00@gmail.com>
2022-01-27 22:35:17 +0900

    ✨  header 구조 작업

--- components/layout/header.tsx
+++ components/layout/header.tsx
@@ -1,5 +1,16 @@
+import { headerRecipe } from "styles/components/layout/header.css";
+
 function Header() {
-	return <header>Header</header>;
+	return (
+		<header className={headerRecipe()}>
+			{/* 서비스 전체보기 버튼 */}
+			<span>SVG</span>
+			{/* 주소 나열 및 클릭시 하단에 주소 설정 팝업 */}
+			<span>광명시 철산동</span>
+			{/* 알림 및 마이페이지 바로가기 */}
+			<span>SVG</span>
+		</header>
+	);
 }
 
 export default Header;
