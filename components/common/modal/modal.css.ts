b4413e780dcf44038f97e7479921ca28de6c5f0e
Bard <prove.ability00@gmail.com>
2022-01-30 20:20:37 +0900

    💄 모달 작업 CSS

---
+++ components/common/modal/modal.css.ts
@@ -0,0 +1,69 @@
+import { style } from "@vanilla-extract/css";
+import { recipe } from "@vanilla-extract/recipes";
+
+// modal
+const modalBodyStyle = style({
+	paddingTop: "10xp",
+});
+
+export const modalBodyRecipe = recipe({
+	base: [modalBodyStyle],
+	variants: {},
+});
+
+// header
+const modalheaderStyle = style({
+	display: "flex",
+	justifyContent: "flex-end",
+	fontSize: "25px",
+});
+
+export const modalHeaderRecipe = recipe({
+	base: [modalheaderStyle],
+	variants: {},
+});
+
+//
+const modalStyle = style({
+	backgroundColor: "white",
+	width: "100%",
+	maxWidth: "739px",
+	height: "100%",
+	borderRadius: "15px",
+	padding: "15px",
+});
+
+export const modalRecipe = recipe({
+	base: [modalStyle],
+	variants: {},
+});
+
+// Overlay
+const modalOverlayStyle = style({
+	zIndex: "9999",
+	position: "absolute",
+	top: "0",
+	left: "0",
+	width: "100%",
+	height: "100%",
+	display: "flex",
+	justifyContent: "center",
+	alignItems: "center",
+	backgroundColor: "rgba(0, 0, 0, 0.3)",
+	transform: "translateY(110%)",
+	transition: "transform 1s ease",
+});
+
+export const modalOverlayRecipe = recipe({
+	base: [modalOverlayStyle],
+	variants: {
+		show: {
+			true: {
+				transform: "translateY(0)",
+			},
+			false: {
+				transform: "translateY(110%)",
+			},
+		},
+	},
+});
