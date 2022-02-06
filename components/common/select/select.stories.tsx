cf9b67e117c61204036b136c7e786a1e38638791
Bard <prove.ability00@gmail.com>
2022-02-06 20:01:54 +0900

    🧪 select story file create

---
+++ components/common/select/select.stories.tsx
@@ -0,0 +1,33 @@
+import { ComponentStory, ComponentMeta } from "@storybook/react";
+import { useState } from "react";
+
+import Select from ".";
+
+export default {
+	title: "Components/Select",
+	component: Select,
+	argTypes: { onChange: { action: "clicked" } },
+} as ComponentMeta<typeof Select>;
+
+//👇 We create a “template” of how args map to rendering
+const Template: ComponentStory<typeof Select> = (args) => {
+	const [value, setValue] = useState<string>("전체");
+	const onChangeValue = (e: any) => setValue(e.target.value);
+	const options = [
+		{ label: "전체", value: "전체" },
+		{ label: "1", value: "1" },
+		{ label: "2", value: "2" },
+		{ label: "3", value: "3" },
+	];
+	return (
+		<Select
+			{...args}
+			value={value}
+			options={options}
+			onChange={onChangeValue}
+		/>
+	);
+};
+
+//👇 Each story then reuses that template
+export const Default = Template.bind({});
