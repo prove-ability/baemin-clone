b6ba01c6df59f398807ef7b4d58f83bb0087da86
Bard <prove.ability00@gmail.com>
2022-02-07 22:29:00 +0900

    🤡 mocking lib setting

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,11 @@
-import "styles/reset.css";
+if (process.env.NEXT_PUBLIC_API_MOCKING === "enabled") {
+	require("../mocks");
+}
+
 import "antd/dist/antd.css";
+import "styles/reset.css";
 
 import type { AppProps } from "next/app";
-
 import Layout from "components/layout";
 
 function MyApp({ Component, pageProps }: AppProps) {
